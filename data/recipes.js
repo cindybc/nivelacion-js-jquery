5d7fedb1acffc52231ee5c10bfc19b18eb761838
cindy <cindialy@gmail.com>
2017-07-10 13:17:18 -0400

    Etapa:4 crear etiketas- recorrer arreglo de objetos- imprimir titulo, autor, img

--- data/recipes.js
+++ data/recipes.js
@@ -7,7 +7,7 @@ var recipesArray = [
     "excerpt": "When I’ve got too many cheese bits on hand, it's now fromage fort to the rescue",
     "source": {
       "name": "David Lebovitz",
-      "url": "http://www.davidlebovitz.com/2014/04/fromage-forte-cheese-dip-spread-recipe/"
+      "url": "img/recipes/320x350/spring-fromage-fort.jpg"
     },
     "cookTime": "15 min",
     "ingredients": [
@@ -153,7 +153,7 @@ var recipesArray = [
     "excerpt": "It is a thing of beauty, shaped into a large oval loaf and glazed with tangy cocktail sauce",
     "source": {
       "name": "Food52",
-      "url": "http://food52.com/recipes/24302-fatted-calf-s-meatloaf"
+      "url": "img/recipes/320x350/spring-meatloaf.jpg"
     },
     "cookTime": "1 hr 30 min",
     "ingredients": [
@@ -313,7 +313,7 @@ var recipesArray = [
     "excerpt": "Juicy, fragrant, perfectly textured for a little fire action",
     "source": {
       "name": "Tartelette",
-      "url": "http://www.tarteletteblog.com/2011/07/recipe-gluten-free-apricots-honey-panna.html"
+      "url": "img/recipes/320x350/summer-apricots-honey-panna-cotta.jpg"
     },
     "cookTime": "35 min",
     "ingredients": [
@@ -422,7 +422,7 @@ var recipesArray = [
     "excerpt": "Making homemade pasta may seem arduous, but this dish is worth the extra work",
     "source": {
       "name": "Martha Stewart",
-      "url": "http://www.marthastewart.com/857657/roasted-butternut-squash-lasagna"
+      "url": "img/recipes/320x350/fall-roasted-butternut-squash-lasagna.jpg"
     },
     "cookTime": "1 hr 35 min",
     "ingredients": [
@@ -572,7 +572,7 @@ var recipesArray = [
     "excerpt": "A good braise always trumps a steak or a roast, both in terms of complex flavors and satisfaction",
     "source": {
       "name": "NY Times",
-      "url": "http://www.nytimes.com/recipes/1014424/italian-red-wine-braised-duck-with-olive-gremolata.html"
+      "url": "img/recipes/320x350/winter-braised-duck.jpg"
     },
     "cookTime": "2 hr 30 min",
     "ingredients": [
@@ -709,7 +709,7 @@ var recipesArray = [
     "excerpt": "A good chocolate tart doesn’t need to be fancy, but it needs to be deep, dark, and yes, somewhat decadent.",
     "source": {
       "name": "David Lebovitz",
-      "url": "http://www.davidlebovitz.com/2011/10/chocolate-tart-recipe/"
+      "url": "img/recipes/320x350/winter-chocolate-tart.jpg"
     },
     "cookTime": "1 hr",
     "ingredients": [
--- js/script-index.js
+++ js/script-index.js
@@ -20,10 +20,10 @@ function printNews(){
 */
 function renderHighlightedRecipes(recipesArray) {
 	console.log('Recipes: ', recipesArray);
-	for(var v in recipesArray){
-		if(recipesArray[v].highlighted){
-			console.log(recipesArray[v]);
-			renderRecipe();
+	for(var recipe in recipesArray){
+		if(recipesArray[recipe].highlighted){
+			console.log(recipesArray[recipe]);
+			renderRecipe(recipesArray[recipe]);
 		}
 	}	
 }
@@ -36,6 +36,34 @@ function renderHighlightedRecipes(recipesArray) {
 */
 function renderRecipe(recipe) {
 	console.log('Voy a pintar la receta: ', recipe);
+    for(var e in recipe){
+    	var titulo = recipe["title"];
+    	console.log(recipe["title"]);
+    	var autor = recipe["source"].name;
+    	console.log(recipe["source"].name);
+    	var url = recipe["source"].url;
+    
+    
+	var enlace = $('<a/>', {'class': 'item-recipe'});
+	var span= $('<span/>',{'class': 'attribution'});
+	var hijo1= $('<span/>',{'class': 'title-recipe'});
+	var hijo2= $('<span/>',{'class': 'metadata-recipe'});
+	var hijo2span1= $('<span/>',{'class': 'author-recipe'});
+	var hijo2span2= $('<span/>',{'class': 'bookmarks-recipe'});
+    var hijo2span21= $('<span/>',{'class': 'icon-bookmark'});
+    var imagen = $('<img/>',{'src': url});
+
+    hijo2span2.append(hijo2span21);
+    hijo2.append(hijo2span2);
+    hijo2.append(hijo2span1);
+    hijo2span1.append(autor);
+    hijo1.append(titulo);
+    span.append(hijo1);
+    span.append(hijo2);
+    enlace.append(span);
+    enlace.append(imagen);
+   }
+   $('.list-recipes').append(enlace);
 }
 
 
